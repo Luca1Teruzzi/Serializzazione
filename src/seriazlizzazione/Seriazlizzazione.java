fff4a6bc65bf311b4f6edbfbf73bb9e1cf79df9a
teruzzi.luca <teruzzi.luca@TAVE-PC34.OSDBSESTO.local>
2021-04-15 14:06:48 +0200

    Add Write and Read

--- src/seriazlizzazione/Seriazlizzazione.java
+++ src/seriazlizzazione/Seriazlizzazione.java
@@ -28,11 +28,11 @@ public class Seriazlizzazione {
         // TODO code application logic here
         Studente a=new Studente("Luca", "Teruzzi", 30);
         try { 
-//            FileOutputStream o=new FileOutputStream("test.ser");
-//            ObjectOutputStream os=new ObjectOutputStream(o);
-//            os.writeObject(9);
-//            os.writeObject("Ciao");
-//            os.writeObject(1.345);
+            FileOutputStream o=new FileOutputStream("test.ser");
+            ObjectOutputStream os=new ObjectOutputStream(o);
+            os.writeObject(9);
+            os.writeObject("Ciao");
+            os.writeObject((float)1.345);
 //            os.writeObject(true);
 //            os.writeObject('a');
 //            os.flush();
@@ -43,7 +43,7 @@ public class Seriazlizzazione {
             System.out.println("v1:"+v1);
             String v2=(String) is.readObject();
             System.out.println("v2:"+v2);
-            Double v3=(Double) is.readObject();
+            Float v3=(Float) is.readObject();
             System.out.println("v3:"+v3);
             
         } catch (FileNotFoundException ex) {
